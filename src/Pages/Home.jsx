22b811b15788d8fdcc1ac5b89982e1b2c863305d
ace <kaurrajvinder736@gmail.com>
2023-07-12 16:53:05 +0530

    initial commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../component/Header'
+import Main from '../component/Landingpage/Main'
+
+function Home() {
+  return (
+    <div>
+        <Main/>
+        
+    </div>
+  )
+}
+
+export default Home
---
+++ src/component/Landingpage/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../Header'
+
+function Main() {
+  return (
+    <div className='w-100 h-100'>
+        <Header/>
+        <section className='container align-middle'>
+        <h1 className='text-start'>THINK VISUAL</h1>
+
+        </section>
+    </div>
+  )
+}
+
+export default Main
