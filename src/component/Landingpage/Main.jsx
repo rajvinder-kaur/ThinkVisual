3eda2ee89953f3bfe59c46dfe026c2c8f179323d
ace <kaurrajvinder736@gmail.com>
2023-07-16 01:26:12 +0530

    comit

--- src/component/Landingpage/Main.jsx
+++ src/component/Landingpage/Main.jsx
@@ -5,44 +5,57 @@ import Row from 'react-bootstrap/Row';
 import ShapeExample from '../Image';
 import Col from 'react-bootstrap/Col';
 import Text2 from '../Text-2/Text2';
-import Footer from '../Footer'
+import picture from "../../assets/child.png"
+// import Footer from '../Footer'
 function Main() {
   return (
-    <div className='w-100 h-100'>
+    <div className='w-100  text-start'>
       <Header1 />
-      {/* <section className='container align-middle'>
-        <h1 className='text-start'>THINK VISUAL</h1>
-      </section> */}
-      <Container className='mt-5 '>
-        <Row>
-          {/* 1st section */}
-          <Col>
-            <div className='mt-5' >
+      {/* <Container className='mt-5 vh-100 '>
+        <Row className=' align-middle' >
+          1st section
+          <Col >
+            <div className='mt-5 ' >
               <h3>Welcome to an immersive world of interactive learning!</h3>
               <p>Explore our platform and unlock the power of visualization in education.</p>
             </div>
           </Col>
-          <Col className=''>
+          <Col>
             <ShapeExample />
           </Col>
         </Row>
-      </Container>
+      </Container> */}
+
+      <table className='mt-5 vh-100 container w-50'>
+        <tr className='  align-middle  '>
+          <td><h1>Welcome to an immersive world of interactive learning!</h1><p>Explore our platform and unlock the power of visualization in education.</p><p>Prepare to embark on a journey where concepts come to life, and learning knows no boundaries.</p></td>
+          <td><ShapeExample /></td>
+        </tr>
+      </table>
+      <style jsx>{`
+              h1{
+                font-size: 4rem;
+              }
+              table tr p{
+                font-size: 2rem;
+              }
+      `}</style>
 
       {/* 2nd section */}
-      <Container className='mt-5 d-inline-flex'>
-        <Row>
-          <Col className=''>
-            <ShapeExample />
-          </Col>
-          <Col >
+      <table className='container vh-100'>
+        <tr className='  align-middle  '> 
+          <td>
+            <img src={picture} alt="" />
+          </td>
+          <td className='  align-middle  '>
             <div className='d-flex'>
               <h5>What features our platform offers?</h5>
             </div>
             <Text2 />
-          </Col>
-        </Row>
-      </Container>
-      <Footer />
+          </td>
+        </tr>
+      </table>
+      {/* <Footer /> */}
     </div >
   )
 }
